7381007bbdaba27482567db27ebf01c4a744bdce
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-09-30 15:40:22 +0100

    it works

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -5,32 +5,40 @@ const app = require("./app.js");
 // API call
 // declaring the variables we will need for the axios call
 let inputBox={}
+let url = "";
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
+const apiKey = process.env.apiKey;
+const ofType = "&of=json&url=";
+const lang = "&lang=en";
+
+let options = {};
+
+const requestOptions=  {
+  method: 'POST',
+  redirect: 'follow'
+};
 async function apiCall(req, res){
 
-    let nUrl = req.body.data.newUrl; 
-    const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
-    const apiKey = process.env.apiKey;
-    const ofType = "&of=json&url=";
-    const lang = "&lang=en";
+  let nUrl = req.body.data.newUrl
+  url = (baseUrl+apiKey+ofType+nUrl+lang)
     // getting the URL from the Client
     
-    console.log("nUrl=>", nUrl);
-    const requestOptions=  {
-      method: 'POST',
-      redirect: 'follow'
-    };
+    
+    options = {
+      method: 'GET',
+      url: url,
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8'
+      },
+    }
     // forming the final url
-    let url = (baseUrl+apiKey+ofType+nUrl+lang);
+    console.log("nUrl=>", nUrl);
     console.log("url=>", url);
-        let options = {
-            method: 'GET',
-            url: url,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-        };
+    app.get("/results", async (req,res) => {    
       // axios call
+      console.log("options=>", options)
+      console.log("requestOptions=>", requestOptions)
       const response = await axios(options, requestOptions)
       try{
         let data = await response.data;
@@ -43,20 +51,19 @@ async function apiCall(req, res){
         inputBox["confidence"]=data.confidence;
         inputBox["irony"]=data.irony;
         console.log("inputBox =>", inputBox);
-         app.get("/results", async (req,res) => {
+         
           // console.log("inputBox=>", inputBox)
           // let inputBox = builtInpBox; ///// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
           console.log("/inputBox) to be sent")
           console.log("inputBox)Server =>", inputBox);
           res.status(200).send(inputBox)
-        })
-        // return inputBox
       }catch(error){
         console.log("error", error)
       }
-       
-      
-};
+    })};
+        // return inputBox
+
+    
 
 
   module.exports = {
