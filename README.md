3ae1eb094fead395fa4e446ce96728d5f89af840
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-10-05 19:15:49 +0100

    uploading again

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-# Webpack Express Example App
-
-This app is getting a url from the U.I. and sends it to an API where it is validated to return some of the texts characteristics. 
-
-It is using Webpack, Express and Jest.
-
-It has been made by Dio Papadopoulos as part of a Udacity course for front end development.
-It has been particularly annoying, as is everything when you are not reading the instructions well before working on it.
-I will not start describing the frustration of making this run with Webpack5. But now it is done! :)
-=======
 # Natural language processing evaluation of news article
 
 ## Description:
@@ -57,4 +46,3 @@ devDependencies:
     "node-fetch": "^2.0.0",
     "regenerator-runtime": "^0.13.9"
 ```
->>>>>>> 233fab45560bc70bd90b295e2c46054423938397
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
@@ -42,48 +41,3 @@ module.exports = {
         })
     ]
 }
-=======
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-            },
-        ],
-       
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Client',
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
->>>>>>> 233fab45560bc70bd90b295e2c46054423938397
